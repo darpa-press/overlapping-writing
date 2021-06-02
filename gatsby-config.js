b5568fe720bc3c253c75d471ffbbed16a4c3694d
Rowan McNaught <rowan@studioskiing.com.au>
2021-06-02 17:21:46 +1000

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,15 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "UA-6339735-3",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+            },
+        },
+        {
             resolve: "gatsby-transformer-remark",
             options: {
                 plugins: [
